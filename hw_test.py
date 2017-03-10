b0abd0440fe021e5d6392ea849fae98efb20bb13
Suzuki Kohei <kohei@sra.co.jp>
2017-03-10 13:47:01 +0900

    add one test case

--- hw_test.py
+++ hw_test.py
@@ -7,3 +7,7 @@ def test_01():
     ret = hello.sub('good')
     assert ret != 'Hello! good'
     assert ret == 'Hello! GOOD'
+
+    ret = hello.sub('GOOD')
+    assert ret != 'Hello! good'
+    assert ret == 'Hello! GOOD'
