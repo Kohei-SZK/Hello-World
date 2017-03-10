7d9b3688d64e0cdc953f97a52bbe398e5ff21203
Suzuki Kohei <kohei@sra.co.jp>
2017-03-10 11:11:49 +0900

    python3 and py.test

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
+# coding: utf-8
+
+def sub(msg):
+    reply = 'Hello! ' + msg.upper()
+    print(reply)
+    return reply
+
 if __name__ == '__main__':
     while True:
-        reply = raw_input('Enter text:')
+        reply = input('Enter text:')
+        ret = sub(reply)
         if reply == 'stop': break
-        print 'Hello! ' + reply.upper()
-    print 'Bye' 
+    print('Bye')
